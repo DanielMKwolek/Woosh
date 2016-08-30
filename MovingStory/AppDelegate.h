a274e0c39f2d761fe04227ef4e525917741a1f14
Daniel Kwolek <Daniel@daniels-macbook-pro.local>
2016-08-30 19:31:26 -0400

    Initial Commit

---
+++ MovingStory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovingStory
+//
+//  Created by Daniel Kwolek on 8/30/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovingStory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovingStory
+//
+//  Created by Daniel Kwolek on 8/30/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
